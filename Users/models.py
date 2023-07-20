d123873cf6c6b2a058a5273c9293f15c6b556b3e
Umesh Kumar <umeshkumar26122002@gmail.com>
2023-07-20 18:04:14 +0530

    Added profile photo in model

--- Users/models.py
+++ Users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MaxLengthValidator, FileExtensionValidator
 from mysite.constants import JNV_MAP_LIST, STATES
 from django.contrib.auth.models import User
 
@@ -28,7 +28,7 @@ class UserCard(models.Model):
     
     default_region_name = "JP" # default 
     
-    user = models.ForeignKey(User,on_delete=models.RESTRICT,primary_key=True,unique=True)
+    user = models.OneToOneField(User, on_delete=models.RESTRICT, primary_key=True)
     region_name = models.CharField(max_length =2, choices=RegionName.choices)
     jnv_name = models.CharField(max_length = 50,choices=[(w,"Jawahar Navodaya Vidyalaya, "+w) for y in JNV_MAP_LIST.values() for w in y])
     
@@ -36,13 +36,21 @@ class UserCard(models.Model):
     role = models.CharField(max_length=1,choices=ROLE_CHOICES)
     year = models.PositiveIntegerField(blank=True,null=True)
     HomeTown = models.CharField(max_length=300,blank=True,null=True)
-    
+    profile_photo = models.ImageField(
+        upload_to='profile_photos/',
+        default='default_profile.png',
+        validators=[
+            MaxLengthValidator(limit_value=100 * 1024, message="The image size should be less than 100KB."),
+            FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'], message="Only PNG and JPG files are allowed."),
+            # ... Other validators if needed ...
+        ]
+    )
     def __str__(self) -> str:
         return "Public Data User:"+self.user.username
     
 class UserPrivateInfo(models.Model):
     
-    user = models.ForeignKey(User,on_delete=models.RESTRICT,primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.RESTRICT, primary_key=True)
     current_location_city = models.CharField(max_length=300,blank=True,null=True)
     current_location_state = models.CharField(max_length=40,choices =[(x,x) for x in STATES],blank=True,null=True)
     current_location_zip = models.PositiveIntegerField(blank=True,null=True)
